fd89295d83c55a85d953059dc45b7c097da61654
Thi hong nhung NGUYEN <thi-nguy@e1r2p14.clusters.42paris.fr>
2022-05-13 14:39:20 +0200

    Start to work on problem_002

--- problem_001.cpp
+++ problem_001.cpp
@@ -1,20 +1,20 @@
-/*
-
+/***************************************************************
 Good morning! Here's your coding interview problem for today.
 This problem was recently asked by Google.
 Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
 For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 Bonus: Can you do this in one pass?
 
-*/
+Easy
+****************************************************************/
 
-/****************************************************/
-//                     Naive solution                 
-// Time: O(n^2)                                     
-// Space: O(1): the space required does not depend  
-// on the size of the input array, so  
-// only constant space is used         
-/****************************************************/
+/****************************************************
+                    Naive solution                 
+Time: O(n^2)                                     
+Space: O(1): the space required does not depend  
+on the size of the input array, so  
+only constant space is used         
+****************************************************/
 
 // #include <iostream>
 
@@ -53,18 +53,18 @@ Bonus: Can you do this in one pass?
 // }
 
 
-/****************************************************/
-//          Two-pass Hash Table solution            
-//  
-//  Time complexity: O(n). We traverse the list 
-//  containing nnn elements exactly twice. Since the 
-//  hash table reduces the lookup time to O(1), the 
-//  overall time complexity is O(n).
+/****************************************************
+            Two-pass Hash Table solution            
  
-//  Space complexity: O(n). The extra space 
-//  required depends on the number of items stored in 
-//  the hash table, which stores exactly n elements.
-/****************************************************/
+Time complexity: O(n). We traverse the list 
+containing nnn elements exactly twice. Since the 
+hash table reduces the lookup time to O(1), the 
+overall time complexity is O(n).
+
+Space complexity: O(n). The extra space 
+required depends on the number of items stored in 
+the hash table, which stores exactly n elements.
+****************************************************/
 
 // #include <iostream>
 // #include <map>
@@ -106,18 +106,18 @@ Bonus: Can you do this in one pass?
 //     return 0;
 // }
 
-/****************************************************/
-//          One-pass Hash Table solution            
-//  
-//  Time complexity: O(n). We traverse the list 
-//  containing n elements once. Since the 
-//  hash table reduces the lookup time to O(1), the 
-//  overall time complexity is O(n).
- 
-//  Space complexity: O(n). The extra space 
-//  required depends on the number of items stored in 
-//  the hash table, which stores exactly n elements.
-/****************************************************/
+/****************************************************
+            One-pass Hash Table solution            
+
+Time complexity: O(n). We traverse the list 
+containing n elements once. Since the 
+hash table reduces the lookup time to O(1), the 
+overall time complexity is O(n).
+
+Space complexity: O(n). The extra space 
+required depends on the number of items stored in 
+the hash table, which stores exactly n elements.
+****************************************************/
 
 #include <iostream>
 #include <map>
